defc408b64441b5ab7d5d3dc68d5e62a01caeb50
Josue Teodoro Moreira <teodoro.josue@protonmail.ch>
2024-05-04 19:25:37 -0300

    doc: add doc strings on fns

--- src/aurelio/grammar.clj
+++ src/aurelio/grammar.clj
@@ -1,26 +1,73 @@
 (ns aurelio.grammar)
 
 (defn build-char-p
+  "Returns a predicate function that can compare given character
+  `c1` against any other character."
   [^Character c1]
   (fn [^Character c2]
     (= (int c1) (int c2))))
 
-(defn build-string-p
-  [^String s1]
-  (fn [^String s2]
-    (= s1 s2)))
+^:rct/test
+(comment
+  ((build-char-p \c) \c)
+  ;; => true
+
+  ((build-char-p \c) \t)
+  ;; => false
+  )
 
 (defn build-or-p
-  [& predicates]
-  (if (empty? predicates)
-    (throw (ex-message "`or` predicate has no inner predicates."))
-    (fn [v]
-      (some #(% v) predicates))))
+  "Returns a predicate function that runs given list of
+  `predicates` against any other value."
+  ([]
+   (throw (Exception. "`or` predicate has no inner predicates.")))
+  ([& predicates]
+   (fn [v]
+     ((complement not-any?) #(% v) predicates))))
+
+^:rct/test
+(comment
+  ((build-or-p
+    (build-char-p \c)
+    (build-char-p \t))
+   \c)
+  ;; => true
+
+  ((build-or-p
+    (build-char-p \c)
+    (build-char-p \t))
+   \v)
+  ;; => false
+
+  (try
+    (build-or-p)
+    (catch Exception e
+      (.getMessage e)))
+  ;; => "`or` predicate has no inner predicates."
+  )
 
 (defn build-range-p
+  "Returns a predicate function that tests whether any other
+  character exists inbetween `start` and `end`.
+
+  `NOTE`: Currently, Aurelio does not support UTF-8 or any other
+  encoding format, only ASCII. Please open a PR incase you need
+  that working ASAP."
   [start end]
   (fn [c]
     (<= (int start) (int c) (int end))))
+
+^:rct/test
+(comment
+  ((build-range-p \a \z) \n)
+  ;; => true
+
+  ((build-range-p \0 \9) \3)
+  ;; => true
+
+  ((build-range-p \a \z) \5)
+  ;; => false
+  )
 
 (defn build-seq*-p
   [predicate]
@@ -34,7 +81,15 @@
    :seq+ build-seq*-p
    :opt build-seq*-p})
 
-(defn sort-sym-refs
+(defn- sort-sym-refs
+  "Given a list of `expressions` (the vals for a grammar table),
+  separates the symbol references and reordes them by reference count.
+
+  A symbol reference is a `keyword` that is not a builtin predicate, a
+  a reference to a symbol created by the user.
+
+  `NOTE`: This algorithm should be refactored in the future. We're currently
+  ignoring circular dependencies. Also, it looks complex :("
   [exprs]
   (reduce
    (fn [acc expr]
@@ -53,6 +108,10 @@
    {} exprs))
 
 (defn order-syms-by-dep
+  "Given a `grammar`, reorders its symbols (i.e. `keywords`) based
+  on their reference count by other symbol's expressions.
+
+  See also: `sort-sym-refs`"
   [grammar]
   (let [sym-refs (sort-sym-refs (vals grammar))]
     (->> (sort-by
